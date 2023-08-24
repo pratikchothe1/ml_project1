d8abb245c4557c84f37588454a3630c23bfa797a
pratik <pratikchothe1@gmail.com>
2023-08-24 16:54:18 +0530

    model trainer

--- src/components/.ipynb_checkpoints/data_ingestion-checkpoint.py
+++ src/components/.ipynb_checkpoints/data_ingestion-checkpoint.py
@@ -54,9 +54,10 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
-    modeltrainer=ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    data_transformation.initiate_data_transformation(train_data,test_data)
+    # train_arr,test_arr,_=
+    # modeltrainer=ModelTrainer()
+    # print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
 
